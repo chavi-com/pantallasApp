d967a051b5fe4e0636c63ce9c277c8597891d837
edizonchavi <edizonchavi@gmail.com>
2022-09-24 09:43:58 -0500

    navenagndo por pantallas app

--- App.js
+++ App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"// aias de la pantalla
+          component = {HomeScreen}
+          options={{title:'Inicio'}}
+        
+        />
+        <Stack.Screen
+
+          name="Profile"
+          component={ProfileScreen}
+          options={{title:'perfil del ususario'}}
+        />
+        <Stack.Screen
+
+          name="Dashboard"
+          component={DeshboardScreen}
+          options={{title:'***********'}}
+        />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+    
+  );
+}
+const HomeScreen = ({ navigation})=>{
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        style={{backgroundColor:'purple', padding:10,borderRadius:10}}
+        onPress={()=>{
+          navigation.navigate('Profile', {name:'edison',username:'edisonchavi',rol:1})
+
+        }
+      }
+      >
+      
+        <Text style={{color:'white', fontSize:20}}>Ir al Perfil De Ususario</Text>
+      
+      
+      </TouchableOpacity>
+      
     </View>
+    
+  );
+}
+const ProfileScreen = ({navigation, route})=>{
+  return(
+    <View style={styles.container}>
+      <Text>Este es el ususario {route.params.username}, con nombre {route.params.name}</Text>
+
+      <TouchableOpacity
+        style={{backgroundColor:'purple', padding:10,borderRadius:10}}
+        onPress={()=>{
+          route.params.rol == 1 ? navigation.navigate('Dashboard') : navigation.navigate('Home')
+
+        }
+      }
+      >
+      
+        <Text style={{color:'white', fontSize:20}}>Ingresar al panel de control</Text>
+      
+      
+      </TouchableOpacity>
+      
+
+      
+    </View>
+
   );
 }
 
+const DeshboardScreen = ({navigation})=>{
+  return(
+    <View style={styles.container}>
+      <Text>Estas como ususario administrador del sistema</Text>
+    </View>
+  );
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
+
+
